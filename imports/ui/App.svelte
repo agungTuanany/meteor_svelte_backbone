aef6ae394d95ce755ab7a2cd676a33d226a58ffa
agungTuanany <agung.tuanany@gmail.com>
2020-07-25 17:49:19 +0700

    fix Client Side stucture

---
+++ imports/ui/App.svelte
@@ -0,0 +1,25 @@
+<script>
+    //....
+    console.log("Hello from client side")
+</script>
+
+<style>
+    * {
+        box-sizing       : border-box;
+    }
+
+    body {
+        display          : flex;
+        flex-direction   : column;
+        align-items      : center;
+        justify-content  : center;
+        text-align       : center;
+        margin           : 0;
+        font-family      : Arial, Helvetica, sans-serif;
+    }
+
+</style>
+
+<div>
+    <h1>Hello from Svelte</h1>
+</div>
